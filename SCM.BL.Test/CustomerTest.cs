783a2deca96fb443a2dc976b7984a2593b13020f
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-05 22:09:38 +0100

    created test to validate customer name

---
+++ SCM.BL.Test/CustomerTest.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+
+namespace SCM.BL.Test
+{
+    public class CustomerTest
+t
+ {
+        [Fact]
+        public void Test1()
+           Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StaticTest()
+        {
+            //arrange
+            var c1 = new Customer();
+            c1.FirstName = "Shola";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Tayo";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Titilayo";
+            Customer.InstanceCount += 1;
+
+            //act
+
+            //assert
+            Assert.Equal(3, Customer.InstanceCount);
+        }
+    }
+}
+
+
+   //act
+
+            //assert
+            Assert.Equal(3, Customer.InstanceCount);
+        }
+    }
+}
