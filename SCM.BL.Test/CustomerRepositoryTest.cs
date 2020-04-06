4afbafc360f73ad674041d37ed3fad1a0091aade
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-06 23:50:31 +0100

    created test file

---
+++ SCM.BL.Test/CustomerRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace SCM.BL.Test
+{
+    public class CustomerRepositoryTest
+    {
+        [Fact]
+        public void RetrieveValid()
+        {
+            // Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "ayenisholah@yahoo.com",
+                FirstName = "Shola",
+                LastName = "Ayeni"
+            };
+
+            // Act
+            var actual = customerRepository.Retrieve(1);
+
+            // Assert
+
+            Assert.Equal(expected.CustomerId, actual.CustomerId);
+            Assert.Equal(expected.EmailAddress, actual.EmailAddress);
+            Assert.Equal(expected.FirstName, actual.FirstName);
+            Assert.Equal(expected.LastName, actual.LastName);
+
+        }
+    }
+}
