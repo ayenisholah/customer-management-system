718ea47518f55a9f56ccb0b366b93acef5a64a57
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-07 00:45:51 +0100

    created order repository class

---
+++ SCM.BL/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace SCM.BL
+{
+    public class OrderRepository
+    {
+        public OrderRepository()
+        {
+        }
+
+        public Order Retrieve(int orderId)
+        {
+            // create the instance of the order class
+            // pass in the requested id
+
+            Order order = new Order(orderId);
+
+            // code that retrieves the defined order
+            // a populated order
+
+            if (orderId == 10)
+            {
+                // use current year in hard-coded data
+                order.OrderDate =
+                    new DateTimeOffset(DateTime.Now.Year, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+            }
+
+            return order;
+        }
+
+        public bool Save(Order order)
+        {
+            return true;
+        }
+    }
+}
