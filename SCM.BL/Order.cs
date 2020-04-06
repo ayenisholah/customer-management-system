44b56025716703df50a6d60c65fd46e342b869e5
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-06 22:52:42 +0100

    created order class

---
+++ SCM.BL/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace SCM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; set; }
+
+        public Order Retrieve(int orderId)
+        {
+            // code that retrieves the specified order
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            // code that saves order
+
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+
+    }
+}
