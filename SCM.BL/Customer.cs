64a0cc70687e1e1593d0e229a106f41ea2f96853
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-08 13:52:26 +0100

    created field for customer types

--- SCM.BL/Customer.cs
+++ SCM.BL/Customer.cs
@@ -18,6 +18,7 @@ namespace SCM.BL
 
         public List<Address> AddressList { get; set; }
         public int CustomerId { get; private set; }
+        public int CustomerType { get; set; }
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
